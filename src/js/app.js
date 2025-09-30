54bd8817a87627bd81f0cddea4f207eca0c4d0d8
Lucas Matias Calvo <lucascalvo2903@gmail.com>
2025-09-30 03:32:53 +0000

    trabajo entregado

--- src/js/app.js
+++ src/js/app.js
@@ -30,20 +30,57 @@ function render(variables = {}) {
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+  document.querySelector("#widget_content").innerHTML = `
+  <div class="widget">
+    ${cover}
+    <img src="${variables.avatarURL}" class="photo" />
+
+    <h1>
+      ${variables.name == null ? "Your name" : variables.name} 
+      ${variables.lastName == null ? "Last name" : variables.lastName}
+    </h1>
+
+    <h2>
+      ${variables.role == null ? "Role" : variables.role}
+    </h2>
+
+    <h3>
+      ${variables.city == null ? "City" : variables.city};
+      ${variables.country == null ? "Country" : variables.country};
+    </h3>
+
+    <ul class="${variables.socialMediaPosition}">
+      <li>
+        <a href="https://twitter.com/${
+          variables.twitter == null ? "twitteruser" : variables.twitter
+        }">
+          <i class="fab fa-twitter"></i>
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/${
+          variables.github == null ? "LucasCalvo-web" : variables.github
+        }">
+          <i class="fab fa-github"></i>
+        </a>
+      </li>
+      <li>
+        <a href="https://linkedin.com/in/${
+          variables.linkedin == null ? "linkedinuser" : variables.linkedin
+        }">
+          <i class="fab fa-linkedin"></i>
+        </a>
+      </li>
+      <li>
+        <a href="https://instagram.com/${
+          variables.instagram == null ? "lucas.matias14" : variables.instagram
+        }">
+          <i class="fab fa-instagram"></i>
+        </a>
+      </li>
+    </ul>
+  </div>
+`;
 }
 
 /**
